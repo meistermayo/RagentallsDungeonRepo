eeb91b981258bbeb46b9b6b3f752046f3e8644da
Luke Desktop <meistermayo@gmail.com>
2018-03-01 16:58:28 -0500

    lights out experiment

---
+++ Ragentall's Dungeon/Assets/LightsOutTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOutTimer : MonoBehaviour {
+    [SerializeField] float timer;
+    [SerializeField] Ragentall_Chase1_Script Ragentall;
+    [SerializeField] GameObject Lights;
+    AudioSource noise;
+    private void Start()
+    {
+        StartCoroutine(TurnOffStuff());
+        noise = GetComponent<AudioSource>();
+    }
+
+    IEnumerator TurnOffStuff()
+    {
+        yield return new WaitForSeconds(timer);
+        noise.Play();
+        Ragentall.enabled = (true);
+        Lights.SetActive(false);
+    }
+}
